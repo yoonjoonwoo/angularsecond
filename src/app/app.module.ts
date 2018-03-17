f6f94caaaae8c3cd4aaa7f72c5714350943786a6
kosmo24 <bl085@naver.com>
2018-03-17 11:06:11 +0900

    2주차 사용자메뉴 만듬?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
 
+
+
+const routes: Routes = [
+  // 사용자 화면
+  {path: '', component: IndexComponent, children:[
+      {path: '', component: HomeComponent},
+      {path: 'jquery', component: JqueryComponent},
+      {path: 'angular', component: AngularComponent}
+    ]},
+  // 참고: 향후 관리자 생성 모듈
+// { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
